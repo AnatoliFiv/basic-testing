6ef1132488ad136115a9add76d253804e9d02599
AnatoliFiv <164253239+AnatoliFiv@users.noreply.github.com>
2025-11-06 22:38:57 +0300

    feat: implement table-driven tests for simpleCalculator

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,25 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  { a: 10, b: 4, action: Action.Subtract, expected: 6 },
+  { a: 12, b: 3, action: Action.Multiply, expected: 36 },
+  { a: 30, b: 2, action: Action.Divide, expected: 15 },
+  { a: 12, b: 2, action: Action.Exponentiate, expected: 144 },
+  { a: 11, b: 11, action: 'wrong action :)', expected: null },
+  { a: '32', b: '', action: Action.Add, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+  test.each(testCases)(
+    'should $action $a and $b to get $expected',
+    ({ a, b, action, expected }) => {
+      const input = { a, b, action };
+      const result = simpleCalculator(input);
+      expect(result).toBe(expected);
+    },
+  );
 });
